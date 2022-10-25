76b75ec9cedd8d9f23e8e8bda92acbab0da4c0a9
David <davidmosquera@uninorte>
2022-10-25 07:59:22 -0500

    Correction

---
+++ Nodo.py
@@ -0,0 +1,44 @@
+
+
+class Nodo:
+    def __init__(self,loc:int, name:str,) -> None:
+        self.loc = loc
+        self.name = name
+        self.prev = None
+        self.next = None
+
+class Propiedad(Nodo):
+    def __init__(self, loc: int, name: str,costo:int,renta:int,color:str) -> None:
+        super().__init__(loc, name)
+        self.costo = costo
+        self.renta = renta
+        self.color = color
+        self.owner = None
+        self.owned = False
+    def __repr__(self) -> str:
+        return f"{self.name} {self.costo}"
+
+class Servicio(Nodo):
+    def __init__(self, loc: int, name: str) -> None:
+        super().__init__(loc, name)
+        self.owner = None
+        self.owned = False
+
+class Ferrocarril(Nodo):
+    def __init__(self, loc: int, name: str) -> None:
+        super().__init__(loc, name)
+        self.owner = None
+        self.owned = False
+
+class Arca(Nodo):
+    def __init__(self, loc: int, name: str,goal: int) -> None:
+        super().__init__(loc, name)
+        self.goal = goal
+
+class Cofre(Nodo):
+    def __init__(self, loc: int, name: str,tipo:int,money:int) -> None:
+        super().__init__(loc, name)
+        if tipo ==0:
+            self.bono = money
+        else:
+            self.cobro = money
